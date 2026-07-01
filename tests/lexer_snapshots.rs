5380a2f8cf8ef8981390d058f64cbb7581781c3c
Nirbhik The Nice <nirbhikkumawat@gmail.com>
2026-07-01 23:22:31 +0530

    added test for lexer

---
+++ tests/lexer_snapshots.rs
@@ -0,0 +1,10 @@
+use insta::{assert_debug_snapshot};
+use toml::lexer::Lexer;
+
+#[test]
+fn test_lexer(){
+    let toml = include_str!("../examples/lexer.toml");
+    let mut lexer = Lexer::new(toml);
+    let tokenized = lexer.tokenize();
+    assert_debug_snapshot!(tokenized);
+}
